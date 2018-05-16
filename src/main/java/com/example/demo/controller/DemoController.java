5dc486306e79e260ae8a348d9de528bdb31f5cfa
liushiyang <651013145@qq.com>
2018-05-16 17:50:15 +0800

    web跳转

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,24 +1,21 @@
 package com.example.demo.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-	@RequestMapping("/hi")
-	public String  hi(HttpServletRequest req,HttpServletResponse res) {
-		try {
-			res.sendRedirect("defaulltDataTables.html");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return "../resources/templates/html/defaulltDataTables.html";
+	@GetMapping("/hi")
+	public String  hi(Model model) {
+//		try {
+//			res.sendRedirect("defaulltDataTables.html");
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		model.addAttribute("hi", "this is my hi");
+		return "html/defaultDataTables";
 	}
 }
