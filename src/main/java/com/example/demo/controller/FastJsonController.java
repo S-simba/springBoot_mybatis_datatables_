93c4abf940b331bcdbbc1af0cff4770e3bb42554
liushiyang <651013145@qq.com>
2018-05-21 18:17:14 +0800

    测试cros，修改部分代码
    Signed-off-by: liushiyang <651013145@qq.com>

--- src/main/java/com/example/demo/controller/FastJsonController.java
+++ src/main/java/com/example/demo/controller/FastJsonController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.Date;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,7 +12,7 @@ import com.example.demo.entity.User;
 @Controller
 @RequestMapping("fastjson")
 public class FastJsonController {
-	@RequestMapping("/test")
+	@RequestMapping("test")
 	@ResponseBody
 	public User test() {
 		User user = new User();
@@ -19,7 +20,7 @@ public class FastJsonController {
 		user.setUserID(0);
 		user.setName("jhon");
 		user.setBirthday(new Date());
-		int i =1/0;
+//		int i =1/0;
 		return user;
 	}
 }
